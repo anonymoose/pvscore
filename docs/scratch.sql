e607ac843a277892cb7d34ba4082310d9501be17
kbedwell <kenneth.bedwell@gmail.com>
2012-10-03 03:47:03 -0400

    missed one

--- docs/scratch.sql
+++ docs/scratch.sql
@@ -3610,3 +3610,30 @@ alter table crm_enterprise add column email varchar(50);
 
 
 select * from crm_customer where email = 'amers_j@yahoo.com';
+
+# index cache rate
+SELECT 
+  sum(idx_blks_read) as idx_read,
+  sum(idx_blks_hit)  as idx_hit,
+  (sum(idx_blks_hit) - sum(idx_blks_read)) / sum(idx_blks_hit) as ratio
+FROM 
+  pg_statio_user_indexes;
+
+# index usage
+SELECT 
+  relname, 
+  100 * idx_scan / (seq_scan + idx_scan) percent_of_times_index_used, 
+  n_live_tup rows_in_table
+FROM 
+  pg_stat_user_tables 
+ORDER BY 
+  n_live_tup DESC;
+
+
+# cache hit rate
+SELECT 
+  sum(heap_blks_read) as heap_read,
+  sum(heap_blks_hit)  as heap_hit,
+  (sum(heap_blks_hit) - sum(heap_blks_read)) / sum(heap_blks_hit) as ratio
+FROM 
+  pg_statio_user_tables;
