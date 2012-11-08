bc305aff46c80af7af7dac9aa041f891d1bc4882
kbedwell <kenneth.bedwell@gmail.com>
2012-11-08 11:52:02 -0500

    fixed exec.sh

--- pvscore/bin/exec.sh
+++ pvscore/bin/exec.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# /apps/wm/app/app/bin/exec.sh wm wm.bin.market_latest import_splits
-# /apps/wm/app/app/bin/exec.sh wm wm.bin.buyins_latest run
+# /apps/pvs/pvscore/pvscore/bin/exec.sh pvs pvs.bin.eye_process process_upload
 
 export ext=$1
 export pkg=$2
@@ -9,13 +8,13 @@ export method=$3
 
 export MPLCONFIGDIR=/tmp
 
-cd /apps/$ext/app
-source ../python/bin/activate
-export PYTHONPATH=/apps/$ext/app:/apps/$ext/extensions
+cd /apps/$ext/$ext
+source ../bin/activate
+export PYTHONPATH=/apps/$ext/$ext:/apps/$ext/pvscore
 export LD_LIBRARY_PATH=/usr/local/pgsql/lib:/usr/local/lib64/R/lib:$LD_LIBRARY_PATH
 export PATH=$PATH:/usr/local/bin
 
-nice python -c "from $pkg import $method; $method($4)" -I ../extensions/$1/prod.ini >> /apps/$ext/log/$pkg.$method.log 2>&1
+nice python -c "from $pkg import $method; $method($4)" -I production.ini >> /apps/$ext/log/$pkg.$method.log 2>&1
 #python -c "from $pkg import $method; $method()" -I ../extensions/$1/prod.ini #>> /tmp/$pkg.$method.log
 
 cd -
