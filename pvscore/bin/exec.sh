d09218357c73c6a382f34d614a0b0a92b30623b6
kbedwell <kenneth.bedwell@gmail.com>
2012-12-02 01:17:07 -0500

    fixed path issue in exec.sh

--- pvscore/bin/exec.sh
+++ pvscore/bin/exec.sh
@@ -8,13 +8,13 @@ export method=$3
 
 export MPLCONFIGDIR=/tmp
 
-cd /apps/$ext/$ext
+cd /apps/pvs/$ext
 source ../bin/activate
-export PYTHONPATH=/apps/$ext/$ext:/apps/$ext/pvscore
+export PYTHONPATH=/apps/pvs/$ext:/apps/pvs/pvscore
 export LD_LIBRARY_PATH=/usr/local/pgsql/lib:/usr/local/lib64/R/lib:$LD_LIBRARY_PATH
 export PATH=$PATH:/usr/local/bin
 
-nice python -c "from $pkg import $method; $method($4)" -I production.ini >> /apps/$ext/log/$pkg.$method.log 2>&1
+nice python -c "from $pkg import $method; $method($4)" -I production.ini >> /apps/pvs/log/$pkg.$method.log 2>&1
 #python -c "from $pkg import $method; $method()" -I ../extensions/$1/prod.ini #>> /tmp/$pkg.$method.log
 
 cd -
