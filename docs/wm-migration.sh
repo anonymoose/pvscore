5d89fa8323e49732fb36b35e63fca28a038a685f
kbedwell <kenneth.bedwell@gmail.com>
2012-12-01 19:53:13 -0500

    updated service starter files

--- docs/wm-migration.sh
+++ docs/wm-migration.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
-set -x
+exit
+#
+# on oldwm
+#
+sudo /etc/init.d/nginx stop
+
+#
+# on web01
+#
+sudo systemctl stop nginx
+
+#
 # on db01, as kbedwell
+#
+
+cd /apps/pvs
+rsync -avz web01-bak.eyefound.it:/apps/pvs/storage /apps/pvs
+chown -R web.web ./storage
 
 cd /apps/pvs/wm
 source ../bin/activate
@@ -26,8 +42,6 @@ gunzip production-wm.sql.gz
 psql -U wm -d wm -f production-wm.sql
 
 
-
-
 psql -U postgres -d wm -f ../pvscore/docs/wm-reduce.sql
 #python ../pvscore/pvscore/bin/delete_enterprise.py retail2 3
 #python ../pvscore/pvscore/bin/uuidkey.py wm wm /Users/kbedwell/dev/pydev/pvs/storage
@@ -49,6 +63,37 @@ psql -U retail -d retail -f constraint-drops.sql
 psql -U retail -d retail -f wm-reduced.sql
 psql -U retail -d retail -f constraint-adds.sql
 
-redis-cli flushdb
-sudo systemctl start pvs.service
+cd /apps/pvs
+rsync -avz web01-bak.eyefound.it:/apps/pvs/storage /apps/pvs
+chown -R web.web ./storage
 
+#
+# on lb01, as root
+#
+vi /etc/pound.cfg
+-- repoint wealthmakers.com to web01-bak
+systemctl restart pound.service
+
+#
+# on web01, as kbedwell
+#
+redis-cli flushdb
+
+cd /apps/pvs/pvscore
+git pull
+cd ../pvs
+git pull
+cd ../wm
+git pull
+
+
+sudo systemctl start pvs.service
+--- check that all is well on eyefound.it and healthustore.net
+--- modify pvs.service and set up wm.service to take port numbers
+--- install nginx wm.conf
+sudo cp /pvs/pvscore/config/prod.web/etc/nginx/sites/*.conf /etc/nginx/site
+sudo systemctl restart nginx
+sudo systemctl start pvs.service
+sudo systemctl enable wm.service
+sudo systemctl start wm.service
+--- check www.wealthmakers
