04a076416191dc19adfadf60fd16166e7b61cffe
kbedwell <kenneth.bedwell@gmail.com>
2012-12-04 10:55:21 -0500

    fixed backup-db.sh for new environment

--- pvscore/bin/backup_db.sh
+++ pvscore/bin/backup_db.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
 set -x
-# backup_db.sh wm wm wm wm02
+# backup_db.sh wm wm 
+# backup_db.sh retail retail
+
+export db=$1
+export pass=$2
 
 
+/usr/bin/pg_dump -U $db -O -c $pass | gzip > /apps/pvs/db/$db.sql.gz
 
-export ext=$1
-export db=$2
-export pass=$3
-export other_server=$4
-export version=$5
-
-
-export LD_LIBRARY_PATH=/usr/local/pgsql/$version/lib:/usr/local/lib64/R/lib:$LD_LIBRARY_PATH
-export PATH=$PATH:/usr/local/bin
-
-/usr/local/pgsql/$version/bin/pg_dump -U $db -O -c $pass | gzip > /apps/$ext/db/$ext.$db.sql.gz
-
-/usr/bin/scp /apps/$ext/db/$ext.$db.sql.gz disco@$other_server:/apps/$ext/db
