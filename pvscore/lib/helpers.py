229a2621538d6032301fec402cabf5499d13b7b6
Ken Bedwell <kenneth.bedwell@gmail.com>
2013-03-17 16:55:39 -0400

    more common add_this buttons

--- pvscore/lib/helpers.py
+++ pvscore/lib/helpers.py
@@ -78,9 +78,9 @@ def add_this_buttons_large(request, style=None):
 <!-- AddThis Button BEGIN -->
 <div class="addthis_toolbox addthis_default_style addthis_32x32_style" style="{style}">
 <a class="addthis_button_pinterest"></a>
-<a class="addthis_button_preferred_2"></a>
-<a class="addthis_button_preferred_3"></a>
-<a class="addthis_button_preferred_4"></a>
+<a class="addthis_button_facebook"></a>
+<a class="addthis_button_twitter"></a>
+<a class="addthis_button_email"></a>
 <a class="addthis_button_compact"></a>
 <a class="addthis_counter addthis_bubble_style"></a>
 </div>
